219f4fb60569adf850b194f3e9ce6d2378523dc9
Matheus Omena <omena.theus@live.com>
2024-07-13 17:54:29 -0300

    build: add font

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import {
+  useFonts,
+  RedHatText_400Regular,
+  RedHatText_700Bold,
+} from '@expo-google-fonts/red-hat-text';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    RedHatText_400Regular,
+    RedHatText_700Bold,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style='auto' />
+    <View>
+      {fontsLoaded ? <Text>Open up App.tsx to start working on your app!</Text> : <View />}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
